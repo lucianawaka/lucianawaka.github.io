84e3c4a7c64b0684faff4be3639c6999db18def5
Luciana Bahia Wakabayashi Fabri <lucianawakabayashi@gmail.com>
2021-06-06 09:22:33 -0300

    iframe power BI

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+<iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=9b554bbf-d666-4e96-959f-b5463e823e85&autoAuth=true&ctid=d0a97625-f863-4b46-92dc-2ad3a7393381&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>
 </body>
 </html>
